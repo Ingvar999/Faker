0bea49e2c15af3982022efe22ba5afbdca31f1c6
Ingvar <shymanski.ihar@gmail.com>
2018-09-26 12:00:31 +0300

    Init. Created basic classes

---
+++ FakerLib/Faker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using DTOLib;
+
+namespace FakerLib
+{
+    public class Faker
+    {
+        public T Create<T>() where T: DTO, new()
+        {
+            T res = new T();
+            FieldInfo[] fields = typeof(T).GetFields();
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            return res;
+        }
+    }
+}
